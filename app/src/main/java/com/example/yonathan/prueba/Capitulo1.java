46a9255ef08c68825b533f30822f68d1ce68d632
Orlando <orlandosanch3z@gmail.com>
2018-11-16 10:24:56 -0800

    10%

--- app/src/main/java/com/example/yonathan/prueba/Capitulo1.java
+++ app/src/main/java/com/example/yonathan/prueba/Capitulo1.java
@@ -2,6 +2,8 @@ package com.example.yonathan.prueba;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -18,10 +20,43 @@ public class Capitulo1 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_capitulo1);
-        mPal1=(EditText) findViewById(R.id.txtpal1);
-        mPal2=(EditText) findViewById(R.id.txtpal2);
-        mPal3=(EditText) findViewById(R.id.txtpal3);
+        mPal1=(EditText)findViewById(R.id.txtpal1);
+        mPal2=(EditText)findViewById(R.id.txtpal2);
+        mPal3=(EditText)findViewById(R.id.txtpal3);
         mScore=(TextView)findViewById(R.id.lblscore);
         mBadscore=(TextView)findViewById(R.id.scorebad);
+        mNext=findViewById(R.id.btnCont);
+
+        mPal1.addTextChangedListener(Cap1);
+        mPal2.addTextChangedListener(Cap1);
+        mPal3.addTextChangedListener(Cap1);
+
+        if(mPal1.getText().toString().equals("volteo")){
+            mPal1.setEnabled(false);
+            mScore = 1 + mScore;
+        }
+
+
     }
+        private TextWatcher Cap1 = new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                  String text1 = mPal1.getText().toString().trim();
+                  String text2 = mPal2.getText().toString().trim();
+                  String text3 = mPal3.getText().toString().trim();
+                  mNext.setEnabled(!text1.isEmpty() && !text2.isEmpty() && !text3.isEmpty());
+              //    mNext.setEnabled(text1.isEmpty() == text2.isEmpty() == text3.isEmpty() == true);
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        };
+
 }
