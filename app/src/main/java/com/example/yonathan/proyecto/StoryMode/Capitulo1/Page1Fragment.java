2f5a9879ce0e85d34e82c4527edb12ed0993c0ce
Orlando <orlandosanch3z@gmail.com>
2018-12-20 16:29:02 -0800

    drag and drop

--- app/src/main/java/com/example/yonathan/proyecto/StoryMode/Capitulo1/Page1Fragment.java
+++ app/src/main/java/com/example/yonathan/proyecto/StoryMode/Capitulo1/Page1Fragment.java
@@ -1,11 +1,15 @@
 package com.example.yonathan.proyecto.StoryMode.Capitulo1;
 
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.content.ClipData;
 import android.os.Bundle;
 import android.support.constraint.ConstraintLayout;
 import android.app.Fragment;
+import android.view.DragEvent;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.animation.Animation;
@@ -24,7 +28,7 @@ public class Page1Fragment extends Fragment {
     public Page1Fragment() {
         // Required empty public constructor
     }
-    TextView paf1;
+    TextView paf1,resp1,resp2,op1,op2;
 Button prueba;
 Mod mod;
 
@@ -39,11 +43,17 @@ public interface Mod{
         paf1.setVisibility(View.GONE);
         prueba=v.findViewById(R.id.perder);
         prueba.setVisibility(View.GONE);
+        resp1=v.findViewById(R.id.oracion1);
+        resp2=v.findViewById(R.id.oracion2);
+        op1=v.findViewById(R.id.respuesta);
+        op2=v.findViewById(R.id.txtresp2);
 
 
-        final Animation anim = AnimationUtils.loadAnimation(getActivity(),R.anim.alpha);
-        ConstraintLayout constraintLayout= v.findViewById(R.id.texto);
 
+
+     //   final Animation anim = AnimationUtils.loadAnimation(getActivity(),R.anim.alpha);
+       // ConstraintLayout constraintLayout= v.findViewById(R.id.texto);
+/*
         constraintLayout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -56,6 +66,7 @@ public interface Mod{
                 else {
 
                 }
+
             }
         });
 
@@ -66,9 +77,34 @@ public interface Mod{
                 mod.hearts(intento);
             }
         });
-
+        */
+        resp2.setOnTouchListener(new ChoiceTouchListener());
+        op1.setOnDragListener(dragListener);
+       // resp1.setOnLongClickListener(longClickListener);
         return v;
     }
+    private final class ChoiceTouchListener implements View.OnTouchListener {
+        @SuppressLint("NewApi")
+        @Override
+        public boolean onTouch(View view, MotionEvent motionEvent) {
+            if (motionEvent.getAction() == MotionEvent.ACTION_DOWN) {
+                /*
+                 * Drag details: we only need default behavior
+                 * - clip data could be set to pass data as part of drag
+                 * - shadow can be tailored
+                 */
+
+                ClipData data = ClipData.newPlainText("","");
+                View.DragShadowBuilder myShadow = new View.DragShadowBuilder(view);
+                view.startDrag(data,myShadow,view,0);
+                return true;
+
+            } else {
+                return false;
+            }
+        }
+    }
+
     @Override
     public void onAttach(Activity activity) {
         super.onAttach(activity);
@@ -77,6 +113,28 @@ public interface Mod{
         }
         catch (Exception e){}
     }
+
+    //drag drop
+    View.OnDragListener dragListener = new View.OnDragListener() {
+        @Override
+        public boolean onDrag(View v, DragEvent event) {
+            int dragEvent = event.getAction();
+            final View vData = (View)event.getLocalState();
+            switch (dragEvent){
+                case DragEvent.ACTION_DRAG_ENTERED:
+                    if (vData.getId() == R.id.oracion1);
+                {
+                 op1.setText("in correcto");
+                }
+                break;
+                case DragEvent.ACTION_DRAG_ENDED:
+                    break;
+                case DragEvent.ACTION_DRAG_EXITED:
+                    break;
+            }
+            return true;
+        }
+    };
 
 
 
